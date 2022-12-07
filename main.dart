1809aca199a91f1d0866532ccc4b6e872f891aa6
Wonhyuk Chung <myLoveSarah@gmail.com>
2022-12-07 18:59:10 +0900

    6. 첫 컬럼 SizedBox 로 감싸기 == 첫줄 간격 설정

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-// 5. mainAxisAlignment: 위치 지정하기
+// 6. 첫 컬럼 SizedBox 로 감싸기 == 첫줄 간격 설정
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,12 +18,15 @@ class MyApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("코딩백조"),
-                Text("2022"),
-              ],
+            SizedBox(
+              height: 150,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("코딩백조"),
+                  Text("2022"),
+                ],
+              ),
             ),
             Text("안녕하세요!"),
             Text("전 고양이를 좋아합니다."),
